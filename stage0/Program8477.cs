d2453dfccee7733cf4f9f10ab36908857343fb14
Shira <shirastern.dev@gmail.com>
2025-10-27 23:57:50 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program8477.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Stage0 
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Wellcome8477();
+            Wellcome9698();
+
+        }
+
+        private static void Wellcome9698()
+        {
+            Console.ReadKey();
+        }
+
+        private static void Wellcome8477()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, wellcome to my first console application", name);
+
+        }
+
+
+    }
+}
+
+
