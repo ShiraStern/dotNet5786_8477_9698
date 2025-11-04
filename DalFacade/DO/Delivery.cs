72bfb4de01b61d32351eb16bd7f12b99bd08fa38
Shira <shirastern.dev@gmail.com>
2025-11-04 17:42:45 +0200

    stage 3.0.0

--- DalFacade/DO/Delivery.cs
+++ DalFacade/DO/Delivery.cs
@@ -2,4 +2,15 @@
 namespace DO;
 public record Delivery
 {
+    int Id,
+    int OrderId,
+    int CourierId,
+    DateTime? PickUpTime = null,
+    DateTime? DeliveryTime = null
+)
+{
+    /// <summary>
+    /// Default constructor for stage 3
+    /// </summary>
+    public Delivery() : this(0,0,0) { }
 }
--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,34 +1,41 @@
 ﻿// <summary>
+
+using DO;
+
 /// Order Entity represents a single order in the system,
 /// containing information about the ordered product, customer,
 /// delivery address, and time stamps.
 /// </summary>
-/// <param name="Id">Order unique ID number.</param>
-/// <param name="CustomerName">Full name of the customer who placed the order.</param>
-/// <param name="CustomerEmail">Email address of the customer.</param>
+/// <param name="Id">Order unique ID number(auto number).</param>
+/// <param name="OrderType">Order type.</param>
+/// <param name="OrderNote">Verbal description of the order content.</param>
 /// <param name="CustomerAddress">Full address of the customer (for billing/contact).</param>
 /// <param name="Latitude">Latitude coordinate of the delivery destination (double).</param>
 /// <param name="Longitude">Longitude coordinate of the delivery destination (double).</param>
-/// <param name="DeliveryNotes">Additional notes or instructions for the delivery.</param>
-/// <param name="OrderDate">The date and time the order was placed.</param>
+/// <param name="CustomerFullName">Full name of the customer who placed the order.</param>
+/// <param name="CustomerPhone">Phone number of the customer.</param>
+/// <param name="OrderProperties"> additional properties about the order.</param>
 /// <param name="ShipDate">The date and time the order was shipped.</param>
 /// <param name="DeliveryDate">The date and time the order was successfully delivered.</param>
+/// <param name="OrderDate">The date and time the order was placed.</param>
 public record Order
 (
     int Id,//כשנעשה את היישות תצורה להוסיף מספר רץ
-    string CustomerName,
-    string CustomerEmail,
+    OrderType OrderType,
+    string OrderNote,
     string CustomerAddress,
-    double? Latitude = null,
-    double? Longitude = null,
-    string? DeliveryNotes = null,
-    DateTime? OrderDate = null,
-    DateTime? ShipDate = null,
-    DateTime? DeliveryDate = null
+    double Latitude ,
+    double Longitude,
+    string CustomerFullName,
+    string CustomerPhone,
+    string? OrderProperties= null,
+    DateTime? OrderDate = DateTime.Now
 )
 {
     /// <summary>
     /// Default constructor for stage 3
     /// </summary>
-    public Order() : this(0, "", "", "") { }
+    public Order() : this(0,OrderType.TypeA,"","",0,0,"","") { }
+    /// להחליף את הערך ברירת מחדל של המספר הרץ לממספר מהקונפיג
 }
+
