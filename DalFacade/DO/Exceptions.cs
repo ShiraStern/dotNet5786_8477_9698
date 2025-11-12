951524ccead6a4d645082ca0ddd884c14183b68c
Shira <shirastern.dev@gmail.com>
2025-11-12 15:55:18 +0200

    stage 3.3.1

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -10,3 +10,8 @@ public class DalAlreadyExistsException : Exception
 {
     public DalAlreadyExistsException(string? message) : base(message) { }
 }
+
+public class DalXMLFileLoadCreateException : Exception
+{
+    public DalXMLFileLoadCreateException(string? message) : base(message) { }
+}
