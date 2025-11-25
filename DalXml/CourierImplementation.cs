b2896def069f48762208ac706999779d310c89de
shulamit ram <shularuya@gmail.com>
2025-11-26 01:56:03 +0200

    stage 3.6 corrected

--- DalXml/CourierImplementation.cs
+++ DalXml/CourierImplementation.cs
@@ -27,26 +27,45 @@ internal class CourierImplementation : ICourier
             );
     }
 
+    private XElement createCourierElement(DO.Courier item)
+    {
+        return new XElement("Courier",
+            new XElement("Id", item.Id),
+            new XElement("FullName", item.FullName),
+            new XElement("Phone", item.Phone),
+            new XElement("Email", item.Email),
+            new XElement("Password", item.Password),
+            new XElement("Active", item.Active),
+            new XElement("MaxDistance", item.MaxDistance),
+            new XElement("DeliveryType", item.DeliveryType),
+            new XElement("EmploymentStartDate", item.EmploymentStartDate)
+        );
+    }
     public void Create(Courier item)
     {
-        XElement? courierElem =
-        XMLTools.LoadListFromXMLElement(Config.s_courier_xml).Elements().FirstOrDefault(st => (int?)st.Element("Id") == item.Id);
-        if (courierElem is not null)
-            throw new DalAlreadyExistsException($"Courier with ID={item.Id} already exists");
-        courierElem!.Add(new XElement("Courier", item));
+        int nextId = Config.NextCourierId;
+        item = item with { Id = nextId };        
+        XElement couriersRootElem = XMLTools.LoadListFromXMLElement(Config.s_courier_xml);        //  טעינת קובץ ה-XML (שורש הנתונים).
+        couriersRootElem.Add(createCourierElement(item));        //  הוספת האלמנט החדש שנוצר מהאובייקט (באמצעות createCourierElement).
+        XMLTools.SaveListToXMLElement(couriersRootElem, Config.s_courier_xml);        // שמירת ה-XElement המעודכן בחזרה לקובץ XML.
     }
 
     public void Delete(int id)
     {
-        List<Courier> Courier = XMLTools.LoadListFromXMLSerializer<Courier>(Config.s_courier_xml);
-        if (Courier.RemoveAll(it => it.Id == id) == 0)
-            throw new DalDoesNotExistException($"Course with ID={id} does not exist");
-        XMLTools.SaveListToXMLSerializer(Courier, Config.s_courier_xml);
+        XElement couriersRootElem = XMLTools.LoadListFromXMLElement(Config.s_courier_xml);        // טעינת קובץ ה-XML
+        XElement? courierElem = couriersRootElem.Elements("Courier")        //מציאת האלמנט למחיקה
+                                                .FirstOrDefault(st => (int?)st.Element("Id") == id);
+        if (courierElem is null)
+        {throw new DalDoesNotExistException($"Courier with ID={id} does not exist");}
+        courierElem.Remove();        // מחיקת האלמנט
+        XMLTools.SaveListToXMLElement(couriersRootElem, Config.s_courier_xml);        // שמירת ה-XElement המעודכן
     }
 
     public void DeleteAll()
     {
-        XMLTools.SaveListToXMLSerializer(new List<Courier>(), Config.s_courier_xml);
+        XElement couriersRootElem = XMLTools.LoadListFromXMLElement(Config.s_courier_xml);
+        couriersRootElem.RemoveAll(); // מחיקת כל הצאצאים של אלמנט השורש
+        XMLTools.SaveListToXMLElement(couriersRootElem, Config.s_courier_xml);
     }
 
     public Courier? Read(int id)
@@ -68,6 +87,12 @@ internal class CourierImplementation : ICourier
 
     public void Update(Courier item)
     {
-        throw new NotImplementedException();
+        XElement couriersRootElem = XMLTools.LoadListFromXMLElement(Config.s_courier_xml);        // טעינת קובץ ה-XML
+        (couriersRootElem.Elements("Courier").FirstOrDefault(st => (int?)st.Element("Id") == item.Id)        // מציאת האלמנט הישן וסילוקו. אם לא נמצא, זורקים חריגה.
+            ?? throw new DalDoesNotExistException($"Courier with ID={item.Id} does Not exist"))
+            .Remove();
+        couriersRootElem.Add(createCourierElement(item));        // הוספת האלמנט המעודכן
+        XMLTools.SaveListToXMLElement(couriersRootElem, Config.s_courier_xml);        //  שמירת ה-XElement מעודכן בחזרה לקובץ XML
+
     }
 }
