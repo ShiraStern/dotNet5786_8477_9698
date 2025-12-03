ee9916d1ee8100e4e6d47e4698e9028801b5b92e
shulamit ram <shularuya@gmail.com>
2025-12-03 18:33:44 +0200

    4.1 and bonos

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -5,8 +5,11 @@ using Dal;
  sealed internal class DalList : IDal
 
 {
-    public static IDal Instance { get; } = new DalList();
-    public DalList() { }  
+    private static readonly Lazy<IDal> _instance =
+        new Lazy<IDal>(() => new DalList(), LazyThreadSafetyMode.ExecutionAndPublication); // זה הבונוס - להוסיף סינגלטאון עצל כלומר פה יש פונקציה שרק כאשר קוראים לה רק אז זה מקצה את האובייקט  
+
+    public static IDal Instance => _instance.Value;
+    private DalList() { }  
     public IOrder Order { get; } = new OrederImplementation();
 
     public ICourier Courier { get; } = new CourierImplementation();
