e6188cbcd6f69768305ca286dfb6f93423afb6c5
shulamit ram <shularuya@gmail.com>
2025-12-07 17:49:43 +0200

    6.4 ENUM

---
+++ BL/BO/Courier.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BO
+{
+    public class Courier
+    {
+        int ID;
+       string  fullName;
+       string  fhoneNember;
+       string  email;
+       string  password;
+       bool active;
+       double maxdistance;
+       ENUM kinddelivery;
+        DateTime 
+    }
+}
