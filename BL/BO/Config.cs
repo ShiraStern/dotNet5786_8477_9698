a3abe8ec6c1f0fe972492ab22637317de8fbd15a
shulamit ram <shularuya@gmail.com>
2025-12-07 19:19:52 +0200

    6.4 config

---
+++ BL/BO/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+public class Config
+{
+    public int MaxRange { get; set; }
+    //TO_DO: //stage 4
+    //add props from DalApi.IConfig that we want to show/change in PL
+    //...
+}
