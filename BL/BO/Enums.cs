3ff2a8a02a8ee29e38f2dfc18206dd25e258f3fe
Shira <shirastern.dev@gmail.com>
2025-12-07 17:51:10 +0200

    adding DeliveryType in enum

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,5 +1,10 @@
 ﻿
-namespace BO
+namespace BO;
+public enum DeliveryType
 {
-   
+    None,
+    Drone,
+    Motorcycle,
+    Car
 }
+
---
+++ BL/BO/OrderInProgress.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace BO;
+
+public class OrderInProgress
+{
+    int Id { init; get; }   
+    string CustomerFullName { init; get; }
+    string Phone { init; get; } 
+    string email { init; get; }
+    string password { init; get; }  
+    bool IsActive { init; get; }
+    double? MaxDeliveryDistance { init; get; }
+    enum DeliveryType { init; get; }  
+    DateTime EmploymentStartDate { init; get; }
+  int 
+    int NumberOfOrdersInProgress { init; get; }
+
+
+
+
+}
+
