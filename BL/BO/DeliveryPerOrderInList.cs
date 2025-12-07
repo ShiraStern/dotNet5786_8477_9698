d352831fd2b6f6c5c6f0c5cb589cbef758209f26
Shira <shirastern.dev@gmail.com>
2025-12-07 19:05:59 +0200

    BO identity

---
+++ BL/BO/DeliveryPerOrderInList.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace BO;
+/// <summary>
+/// Represents the details of a delivery associated with an order in a list.
+/// </summary>
+/// <remarks>This class provides information about a specific delivery, including its unique identifier,  the
+/// courier responsible for the delivery, the type of delivery, and its start and end times.  It also includes the
+/// termination status of the delivery.</remarks>
+public class DeliveryPerOrderInList
+{
+    int DeliveryId { get; init; }
+    int courierId { get; init; }    
+    string CourierName { get; init; }
+    DeliveryType DeliveryType { get; init; }
+    DateTime DeliveryStart { get; init; }
+    DeliveryTermintionType deliveryTermintionType { get; set; } 
+    DateTime? DeliveryEndTime { get; set; }
+}
