88633802c1b316c8b5dbf3ef5aaf5a98164387ca
Shira <shirastern.dev@gmail.com>
2025-12-22 19:46:08 +0200

    bl test

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -12,6 +12,15 @@ public enum DeliveryType
     Car
 }
 
+public enum OrderProperties
+{
+    None,
+    Fragile, //שביר
+    Weighty,// כבד משקל
+    FragileAndWeighty //כבד ושביר
+}
+
+
 /// <summary>
 /// Enum representing order types/sizes.
 /// </summary>
@@ -100,7 +109,7 @@ public enum filterOrdersByProperty
 }
  public enum AdminMenuOptions
 {
-    Exit,
+    Exit ,
     GetClock,
     ForwardClock,
     ResetDataBase,
@@ -130,4 +139,20 @@ public enum MainMenuOptions
     ForwardClock,
     SetConfig,
     AdminMenu,
+}
+
+public enum OrderMenuOptions
+{
+    Exit=1,
+    GetOrdersStatusCounts,
+    GetOrderList,
+    GetDetails,
+    UpdateDetails,
+    CancelOrder,
+    Delete,
+    AddOrder,
+    EndOrderHandle,
+    HandleOrder,
+    GetClosedDeliveriesPerCourier,
+    GetDeliveriesPerCourier
 }
--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -7,6 +7,151 @@ using Helpers;
 internal class Program
 {
     static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+
+    private static void GetDeliveriesPerCourier()
+    {
+        Console.WriteLine("Please enter the ID of applicant and courier ID");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int courierID = int.Parse(Console.ReadLine()!);
+        s_bl.order.GetDeliveriesPerCourier(applicantID, courierID);
+    }//
+    private static void GetClosedDeliveriesPerCourier()
+    {
+        Console.WriteLine("Please enter the ID of applicant and courier ID");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int courierID = int.Parse(Console.ReadLine()!);
+        s_bl.order.GetClosedDeliveriesPerCourier(applicantID, courierID);
+    }//
+
+    private static void EndOrderHandle()
+    {
+        Console.WriteLine("Please enter the ID of applicant, courier, order and delivery");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int courierID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        int deliveryID = int.Parse(Console.ReadLine()!);
+        s_bl.order.EndOrderHandle(applicantID, courierID, orderID, deliveryID);
+    }//
+    private static void AddOrder()
+    {
+        Console.WriteLine("Please enter the ID of applicant and values of order to add");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        // create BO.Order
+        Console.WriteLine();
+        int id = int.Parse(Console.ReadLine()!);
+        BO.OrderType orderType = (BO.OrderType)int.Parse(Console.ReadLine()!);
+        string VerbalDescription = Console.ReadLine()!;
+        string FullAddressOfTheOrder = Console.ReadLine()!;
+        double AirDistance = double.Parse(Console.ReadLine()!);
+        string FullNameOfTheInviter = Console.ReadLine()!;
+        string OrderersPhoneNumber = Console.ReadLine()!;
+        BO.OrderProperties orderProperties = (BO.OrderProperties)int.Parse(Console.ReadLine()!);
+        DateTime OrderOpeningTime = DateTime.Parse(Console.ReadLine()!);
+        DateTime EstimatedDeliveryTime = DateTime.Parse(Console.ReadLine()!);
+        DateTime MaximumDeliveryTime = DateTime.Parse(Console.ReadLine()!);
+        BO.OrderStatus OrderStatus = (BO.OrderStatus)int.Parse(Console.ReadLine()!);
+        BO.ScheduleStatus ScheduleStatus = (ScheduleStatus)int.Parse(Console.ReadLine()!);
+        int TimeLeftToCompleteOrder = int.Parse(Console.ReadLine()!);
+        BO.Order order = new BO.Order
+        {
+            ID = id,
+            OrderType = orderType,
+            VerbalDescription = VerbalDescription,
+            FullAddressOfTheOrder = FullAddressOfTheOrder,
+            AirDistance = AirDistance,
+            FullNameOfTheInviter = FullNameOfTheInviter,
+            OrderersPhoneNumber = OrderersPhoneNumber,
+            OrderProperties = orderProperties,
+            OrderOpeningTime = OrderOpeningTime,
+            EstimatedDeliveryTime = EstimatedDeliveryTime,
+            MaximumDeliveryTime = MaximumDeliveryTime,
+            OrderStatus = OrderStatus,
+            ScheduleStatus = ScheduleStatus,
+            TimeLeftToCompleteOrder = TimeSpan.FromDays(TimeLeftToCompleteOrder)
+        };
+        //  להוסיף חישוב של קאורדינטות
+        s_bl.order.UpdateDetails(applicantID, order);
+    }//
+    private static void DeleteOrder()
+    {
+        Console.WriteLine("Please enter the ID of applicant and order ID");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        s_bl.order.Delete(applicantID, orderID);
+    }//
+
+    private static void CancelOrder()
+    {
+        Console.WriteLine("Please enter the ID of applicant and order ID");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        s_bl.order.CancelOrder(applicantID, orderID);
+    }//
+    private static void UpdateOrderDetails()
+    {
+        Console.WriteLine("Please enter the ID of applicant and ID of order to update");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        // create BO.Order
+        Console.WriteLine();
+        int id= int.Parse(Console.ReadLine()!);
+        BO.OrderType orderType= (BO.OrderType)int.Parse(Console.ReadLine()!);
+        string VerbalDescription = Console.ReadLine()!;
+        string FullAddressOfTheOrder= Console.ReadLine()!;
+        double AirDistance = double.Parse(Console.ReadLine()!);
+        string FullNameOfTheInviter= Console.ReadLine()!;
+        string OrderersPhoneNumber = Console.ReadLine()!;
+        BO.OrderProperties orderProperties = (BO.OrderProperties)int.Parse(Console.ReadLine()!);
+        DateTime OrderOpeningTime= DateTime.Parse(Console.ReadLine()!);
+        DateTime EstimatedDeliveryTime= DateTime.Parse(Console.ReadLine()!);
+        DateTime MaximumDeliveryTime= DateTime.Parse(Console.ReadLine()!);
+        BO.OrderStatus OrderStatus= (BO.OrderStatus)int.Parse( Console.ReadLine()!);    
+        BO.ScheduleStatus ScheduleStatus= (ScheduleStatus)int.Parse( Console.ReadLine()!);
+        int TimeLeftToCompleteOrder= int.Parse(Console.ReadLine()!);
+        BO.Order order = new BO.Order
+        {
+            ID = id,
+            OrderType = orderType,
+            VerbalDescription = VerbalDescription,
+            FullAddressOfTheOrder = FullAddressOfTheOrder,
+            AirDistance = AirDistance,
+            FullNameOfTheInviter = FullNameOfTheInviter,
+            OrderersPhoneNumber = OrderersPhoneNumber,
+            OrderProperties = orderProperties,
+            OrderOpeningTime = OrderOpeningTime,
+            EstimatedDeliveryTime = EstimatedDeliveryTime,
+            MaximumDeliveryTime = MaximumDeliveryTime,
+            OrderStatus = OrderStatus,
+            ScheduleStatus = ScheduleStatus,
+            TimeLeftToCompleteOrder = TimeSpan.FromDays(TimeLeftToCompleteOrder)
+        };
+        //  להוסיף חישוב של קאורדינטות
+        s_bl.order.UpdateDetails(applicantID, order);
+    }//
+
+    private static void GetOrderDetails()
+    {
+        Console.WriteLine("Please enter the ID of applicant and order ID");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        int orderID = int.Parse(Console.ReadLine()!);
+        s_bl.order.GetDetails(applicantID, orderID);
+    }//
+    private static void GetOrderList()
+    {
+        Console.WriteLine("Please enter the ID of applicant");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        s_bl.order.GetOrderList(applicantID);
+    }//
+    private static void GetOrdersStatusCounts()
+    {
+        Console.WriteLine("Please enter the ID of applicant");
+        int applicantID = int.Parse(Console.ReadLine()!);
+        s_bl.order.GetOrdersStatusCounts(applicantID);
+    }//
+
     private static void UpdateCourierDetails()
     {
     //    Console.WriteLine("Please enter the ID of applicant and ID of specipic courier");
@@ -246,41 +391,58 @@ internal class Program
             {
                 Console.WriteLine(@$"Please enter a number to choose an action:
 1-Exit
-2-AddDelivery
-3-ViewDeliveries
-4-viewAllDeliveries
-5-UpdateDelivery
-6-DeleteDelivery
-7-DeleteAllDeliveries");
+2-Get Orders Status Counts
+3-Get Order List
+4-Get Details
+5-Update Details
+6-Cancel Order
+7-Delete Order
+8- Add Order
+9- End Order Handle
+10-Handle Order
+11-Get Closed Deliveries Per Courier
+12-Get Deliveries Per Courier");
                 choice = int.Parse(Console.ReadLine()!);
             } while (choice is null);
-            switch ((DeliveryMenu)choice)
+            switch ((OrderMenuOptions)choice)
             {
-                case DeliveryMenu.Exit:
-                    //endProgram
+                case OrderMenuOptions.Exit:
+                    continueLoop = false;
                     break;
-                case DeliveryMenu.AddDelivery:
-                    addDelivery();
+                case OrderMenuOptions.GetOrdersStatusCounts:
+                    GetOrdersStatusCounts();
                     break;
-                case DeliveryMenu.ViewDelivery:
-                    viewDelivery();
+                case OrderMenuOptions.GetOrderList:
+                    GetOrderList();
                     break;
-                case DeliveryMenu.ViewAllDeliveries:
-                    viewAllDeliveries();
+                case OrderMenuOptions.GetDetails:
+                    GetOrderDetails();
                     break;
-                case DeliveryMenu.UpdateDelivery:
-                    updateDelivery();
+                case OrderMenuOptions.UpdateDetails:
+                    UpdateOrderDetails();
                     break;
-                case DeliveryMenu.DeleteDelivery:
-                    deleteDelivery();
+                case OrderMenuOptions.CancelOrder:
+                    CancelOrder();
                     break;
-                case DeliveryMenu.DeleteAllDeliveries:
-                    deleteAllDeliveries();
+                case OrderMenuOptions.Delete:
+                    DeleteOrder();
                     break;
-                default:
-                    Console.WriteLine();
+                case OrderMenuOptions.AddOrder:
+                    AddOrder();
                     break;
+                case OrderMenuOptions.EndOrderHandle:
+                    EndOrderHandle();
+                    break;
+                case OrderMenuOptions.GetClosedDeliveriesPerCourier:
+                    GetClosedDeliveriesPerCourier();
+                    break;
+                case OrderMenuOptions.GetDeliveriesPerCourier:
+                    GetDeliveriesPerCourier();
+                    break;
+
+
             }
+
         } while (continueLoop);
     }//
     private static int mainMenuOptions()
@@ -356,5 +518,4 @@ internal class Program
 
     }
 
-}
 }
