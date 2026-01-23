0c1cd13ceac1ca2f437e415992e60a9fcb9ac630
shulamit ram <shularuya@gmail.com>
2026-01-23 12:49:49 +0200

    login

--- PL/Courier/CourierWindow.xaml.cs
+++ PL/Courier/CourierWindow.xaml.cs
@@ -12,6 +12,8 @@ namespace PL.Courier
 
         private int _applicantId;
 
+        private bool IsAddMode;   //  מצב המסך: true = הוספה, false = עדכון
+
         public string ButtonText { get; set; }
 
         // Dependency Property עבור כל האובייקט
@@ -47,35 +49,36 @@ namespace PL.Courier
             if (CurrentCourier!.ID != 0)
                 s_bl.Courier.AddObserver(CurrentCourier.ID, RefreshCourier);
         }
-
-        public CourierWindow(int courierId = 0)
+        public CourierWindow(int userId, int courierId = 0)
         {
             InitializeComponent();
-
+            _applicantId = userId;
             if (courierId == 0)
             {
                 // מצב הוספה
+                IsAddMode = true;
+
                 CurrentCourier = new BO.Courier
                 {
                     ID = 0
-                    // ערכי ברירת מחדל נוספים אם צריך
                 };
                 ButtonText = "Add";
             }
             else
             {
                 // מצב עדכון
-                CurrentCourier = s_bl.Courier.GetDetails(managerId,courierId);
+                IsAddMode = false;
+
+                CurrentCourier = s_bl.Courier.GetDetails(managerId, courierId);
                 ButtonText = "Update";
             }
 
             DataContext = this;
 
             this.Loaded += CourierWindow_Loaded;
-
             this.Closing += CourierWindow_Closing;
-
         }
+
 
 
         private void RefreshCourier()
@@ -108,7 +111,7 @@ namespace PL.Courier
                     return;
                 }
 
-                if (CurrentCourier.ID == 0)
+                if (IsAddMode)
                 {
                     // הוספה
                     s_bl.Courier.AddCourier(_applicantId, CurrentCourier);
